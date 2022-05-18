76da879c949568a101409883b1eeef6df131b9f4
IDH <megamemefy@gmail.com>
2022-05-19 01:31:11 +0900

    [Revising the Select Query 1] solved

--- HackerRank/Revising_Select_1.sql
+++ HackerRank/Revising_Select_1.sql
@@ -1,5 +1,5 @@
 # https://www.hackerrank.com/challenges/revising-the-select-query/problem?isFullScreen=true
 # Q: Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
 
-# passed solution
+# 통과 문제풀이
 SELECT * FROM CITY WHERE (POPULATION>100000 AND COUNTRYCODE = 'USA');
