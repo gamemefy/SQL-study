4698f217eaeca465c1b78f6faaa429be863df1f6
IDH <megamemefy@gmail.com>
2022-05-19 02:26:39 +0900

    [Weather Observation Station 1] solved

---
+++ HackerRank/Weather_Obsevation_1.sql
@@ -0,0 +1,5 @@
+# https://www.hackerrank.com/challenges/weather-observation-station-1/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+# Query a list of CITY and STATE from the STATION table.
+
+# 통과 문제풀이
+SELECT STATE, STATION FROM CITY;
