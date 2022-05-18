fed82a5d41939a72d3c5f76de4e37b739920da76
IDH <megamemefy@gmail.com>
2022-05-19 02:08:17 +0900

    [Select By ID] solved

---
+++ HackerRank/Select_by_ID.sql
@@ -0,0 +1,5 @@
+# https://www.hackerrank.com/challenges/select-by-id/problem?isFullScreen=true
+# Query all columns for a city in CITY with the ID 1661.
+
+# 통과 문제풀이
+SELECT * FROM CITY WHERE ID = 1661; # 여기서 '1661'로 해도 통과됨
