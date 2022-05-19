378ae89a7454317a7ac2f7a5f343c83026c76d7a
IDH <megamemefy@gmail.com>
2022-05-20 03:29:01 +0900

    [Employee Names] solved

---
+++ HackerRank/Employee_Names.sql
@@ -0,0 +1,5 @@
+# https://www.hackerrank.com/challenges/name-of-employees/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
+# Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+
+# 통과 문제풀이
+SELECT name FROM Employee ORDER BY name;
