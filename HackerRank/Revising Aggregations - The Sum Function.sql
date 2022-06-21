d7e6b2bbf188bfbde435db7af853f4f4e7e461f2
dh <megamemefy@gmail.com>
2022-06-21 16:31:21 +0900

    [Revising Aggregations - The Sum Function] solved

---
+++ HackerRank/Revising Aggregations - The Sum Function.sql
@@ -0,0 +1,4 @@
+# https://www.hackerrank.com/challenges/revising-aggregations-sum/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
+
+# 통과 문제풀이
+SELECT SUM(POPULATION) FROM CITY WHERE DISTRICT = 'California';
