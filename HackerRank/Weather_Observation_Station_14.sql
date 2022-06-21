0abe0e9719d2172e396132febf7a037d040d6f4a
dh <megamemefy@gmail.com>
2022-06-21 16:09:06 +0900

    [Weather Observation Station 14] solved

---
+++ HackerRank/Weather_Observation_Station_14.sql
@@ -0,0 +1,4 @@
+# https://www.hackerrank.com/challenges/weather-observation-station-14/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
+
+# 통과 문제풀이
+SELECT ROUND(MAX(LAT_N),4) FROM STATION WHERE LAT_N < 137.2345;
